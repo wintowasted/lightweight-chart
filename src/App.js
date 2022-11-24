0ceb9662e8b3d44682a4a755837e3957db0161e7
wintowasted <84101286+wintowasted@users.noreply.github.com>
2022-11-24 21:11:22 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { createChart } from "lightweight-charts";
+import { CANDLE_DATA } from "./candle_data";
+import { useEffect, useRef } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const chartRef = useRef();
+
+  useEffect(() => {
+    // Candle Series
+    const chart = createChart(chartRef.current, { width: 1200, height: 600 });
+    const candleSeries = chart.addCandlestickSeries();
+    candleSeries.setData(CANDLE_DATA);
+   
+    // Line Series for SMA Indicator
+    const smaData = calculateSMA(CANDLE_DATA, 10);
+    const smaLine = chart.addLineSeries({
+      color: "rgb(170, 50, 181)",
+      lineWidth: 2,
+    });
+    smaLine.setData(smaData);
+
+    chart.timeScale().fitContent();
+    
+    return () => {
+      chart.remove();
+    };
+  }, []);
+
+  function calculateSMA(data, count) {
+    var avg = function (data) {
+      var sum = 0;
+      for (var i = 0; i < data.length; i++) {
+        sum += data[i].close;
+      }
+      return sum / data.length;
+    };
+    var result = [];
+    for (var i = count - 1, len = data.length; i < len; i++) {
+      var val = avg(data.slice(i - count + 1, i));
+      result.push({ time: data[i].time, value: val });
+    }
+    return result;
+  }
+
+  return <div className="App" ref={chartRef}></div>;
 }
 
 export default App;
---
+++ src/candle_data.js
@@ -0,0 +1,1136 @@
+export const CANDLE_DATA = [
+    {
+      time: 1651258800000,
+      open: 78,
+      high: 78,
+      low: 75,
+      close: 78
+    },
+    {
+      time: 1651262400000,
+      open: 78.5,
+      high: 78.5,
+      low: 74.99,
+      close: 76.9
+    },
+    {
+      time: 1651266000000,
+      open: 77.4,
+      high: 77.4,
+      low: 75,
+      close: 75
+    },
+    {
+      time: 1651269600000,
+      open: 75,
+      high: 75,
+      low: 73.99,
+      close: 73.99
+    },
+    {
+      time: 1651273200000,
+      open: 73.13,
+      high: 73.13,
+      low: 72,
+      close: 72
+    },
+    {
+      time: 1651276800000,
+      open: 72,
+      high: 74.9,
+      low: 72,
+      close: 74.9
+    },
+    {
+      time: 1651280400000,
+      open: 74.9,
+      high: 74.9,
+      low: 74,
+      close: 74
+    },
+    {
+      time: 1651284000000,
+      open: 74,
+      high: 74,
+      low: 73.9,
+      close: 73.9
+    },
+    {
+      time: 1651287600000,
+      open: 73.5,
+      high: 73.5,
+      low: 73,
+      close: 73.31
+    },
+    {
+      time: 1651291200000,
+      open: 73.31,
+      high: 73.31,
+      low: 72,
+      close: 72
+    },
+    {
+      time: 1651294800000,
+      open: 72,
+      high: 72,
+      low: 69.96,
+      close: 69.96
+    },
+    {
+      time: 1651298400000,
+      open: 69.69,
+      high: 69.69,
+      low: 67,
+      close: 67
+    },
+    {
+      time: 1651302000000,
+      open: 67,
+      high: 67,
+      low: 67,
+      close: 67
+    },
+    {
+      time: 1651305600000,
+      open: 67,
+      high: 69.69,
+      low: 67,
+      close: 69.69
+    },
+    {
+      time: 1651309200000,
+      open: 69.69,
+      high: 69.69,
+      low: 69.69,
+      close: 69.69
+    },
+    {
+      time: 1651312800000,
+      open: 69.69,
+      high: 69.69,
+      low: 69,
+      close: 69
+    },
+    {
+      time: 1651316400000,
+      open: 69,
+      high: 69,
+      low: 69,
+      close: 69
+    },
+    {
+      time: 1651320000000,
+      open: 69,
+      high: 69,
+      low: 67,
+      close: 67
+    },
+    {
+      time: 1651323600000,
+      open: 67,
+      high: 67,
+      low: 64.69,
+      close: 64.69
+    },
+    {
+      time: 1651327200000,
+      open: 64.69,
+      high: 64.69,
+      low: 64.69,
+      close: 64.69
+    },
+    {
+      time: 1651330800000,
+      open: 67,
+      high: 67,
+      low: 66,
+      close: 66
+    },
+    {
+      time: 1651334400000,
+      open: 66,
+      high: 66,
+      low: 64,
+      close: 66
+    },
+    {
+      time: 1651338000000,
+      open: 66,
+      high: 66.69,
+      low: 66,
+      close: 66.69
+    },
+    {
+      time: 1651341600000,
+      open: 66.69,
+      high: 66.69,
+      low: 63,
+      close: 66.69
+    },
+    {
+      time: 1651345200000,
+      open: 66.69,
+      high: 66.69,
+      low: 66.66,
+      close: 66.66
+    },
+    {
+      time: 1651348800000,
+      open: 66.66,
+      high: 66.66,
+      low: 66.66,
+      close: 66.66
+    },
+    {
+      time: 1651352400000,
+      open: 66.66,
+      high: 66.66,
+      low: 66.66,
+      close: 66.66
+    },
+    {
+      time: 1651356000000,
+      open: 66.66,
+      high: 66.66,
+      low: 66.66,
+      close: 66.66
+    },
+    {
+      time: 1651359600000,
+      open: 66.66,
+      high: 66.66,
+      low: 66.66,
+      close: 66.66
+    },
+    {
+      time: 1651363200000,
+      open: 66.66,
+      high: 66.66,
+      low: 66.66,
+      close: 66.66
+    },
+    {
+      time: 1651366800000,
+      open: 66.66,
+      high: 66.66,
+      low: 66.66,
+      close: 66.66
+    },
+    {
+      time: 1651370400000,
+      open: 66.66,
+      high: 66.66,
+      low: 66.66,
+      close: 66.66
+    },
+    {
+      time: 1651374000000,
+      open: 66.66,
+      high: 66.66,
+      low: 66.66,
+      close: 66.66
+    },
+    {
+      time: 1651377600000,
+      open: 66.66,
+      high: 66.66,
+      low: 66.66,
+      close: 66.66
+    },
+    {
+      time: 1651381200000,
+      open: 66.66,
+      high: 66.66,
+      low: 66.66,
+      close: 66.66
+    },
+    {
+      time: 1651384800000,
+      open: 66.66,
+      high: 66.66,
+      low: 66.66,
+      close: 66.66
+    },
+    {
+      time: 1651388400000,
+      open: 66.66,
+      high: 66.66,
+      low: 65.98,
+      close: 65.98
+    },
+    {
+      time: 1651392000000,
+      open: 65.98,
+      high: 65.98,
+      low: 65.98,
+      close: 65.98
+    },
+    {
+      time: 1651395600000,
+      open: 65.98,
+      high: 65.98,
+      low: 65.98,
+      close: 65.98
+    },
+    {
+      time: 1651399200000,
+      open: 65.98,
+      high: 65.98,
+      low: 63,
+      close: 65.98
+    },
+    {
+      time: 1651402800000,
+      open: 65.98,
+      high: 65.98,
+      low: 65.7,
+      close: 65.7
+    },
+    {
+      time: 1651406400000,
+      open: 65.7,
+      high: 65.7,
+      low: 63,
+      close: 63
+    },
+    {
+      time: 1651410000000,
+      open: 63,
+      high: 63,
+      low: 63,
+      close: 63
+    },
+    {
+      time: 1651413600000,
+      open: 63,
+      high: 63,
+      low: 62,
+      close: 62
+    },
+    {
+      time: 1651417200000,
+      open: 62,
+      high: 62,
+      low: 62,
+      close: 62
+    },
+    {
+      time: 1651420800000,
+      open: 60,
+      high: 62,
+      low: 60,
+      close: 60
+    },
+    {
+      time: 1651424400000,
+      open: 57,
+      high: 60,
+      low: 57,
+      close: 58
+    },
+    {
+      time: 1651428000000,
+      open: 59,
+      high: 60,
+      low: 57,
+      close: 60
+    },
+    {
+      time: 1651431600000,
+      open: 60,
+      high: 60,
+      low: 57.9,
+      close: 57.9
+    },
+    {
+      time: 1651435200000,
+      open: 59.97,
+      high: 59.97,
+      low: 59.97,
+      close: 59.97
+    },
+    {
+      time: 1651438800000,
+      open: 59.97,
+      high: 59.97,
+      low: 55,
+      close: 57
+    },
+    {
+      time: 1651442400000,
+      open: 56,
+      high: 57,
+      low: 53,
+      close: 56
+    },
+    {
+      time: 1651446000000,
+      open: 53,
+      high: 59.96,
+      low: 53,
+      close: 58
+    },
+    {
+      time: 1651449600000,
+      open: 56.5,
+      high: 59.5,
+      low: 56.5,
+      close: 59.5
+    },
+    {
+      time: 1651453200000,
+      open: 59,
+      high: 59,
+      low: 57,
+      close: 57
+    },
+    {
+      time: 1651456800000,
+      open: 57,
+      high: 57,
+      low: 55,
+      close: 57
+    },
+    {
+      time: 1651460400000,
+      open: 57,
+      high: 57,
+      low: 57,
+      close: 57
+    },
+    {
+      time: 1651464000000,
+      open: 54,
+      high: 59,
+      low: 54,
+      close: 59
+    },
+    {
+      time: 1651467600000,
+      open: 59,
+      high: 64.69,
+      low: 59,
+      close: 64.69
+    },
+    {
+      time: 1651471200000,
+      open: 64.69,
+      high: 67,
+      low: 64.69,
+      close: 67
+    },
+    {
+      time: 1651474800000,
+      open: 67,
+      high: 67,
+      low: 67,
+      close: 67
+    },
+    {
+      time: 1651478400000,
+      open: 67,
+      high: 67,
+      low: 67,
+      close: 67
+    },
+    {
+      time: 1651482000000,
+      open: 66,
+      high: 66,
+      low: 66,
+      close: 66
+    },
+    {
+      time: 1651485600000,
+      open: 66,
+      high: 66,
+      low: 64.5,
+      close: 64.5
+    },
+    {
+      time: 1651489200000,
+      open: 64.5,
+      high: 64.5,
+      low: 64.4,
+      close: 64.4
+    },
+    {
+      time: 1651492800000,
+      open: 64,
+      high: 64,
+      low: 63,
+      close: 63
+    },
+    {
+      time: 1651496400000,
+      open: 63,
+      high: 63,
+      low: 61,
+      close: 62.95
+    },
+    {
+      time: 1651500000000,
+      open: 62.95,
+      high: 63,
+      low: 61.9,
+      close: 63
+    },
+    {
+      time: 1651503600000,
+      open: 63,
+      high: 64,
+      low: 63,
+      close: 64
+    },
+    {
+      time: 1651507200000,
+      open: 64,
+      high: 64.9,
+      low: 63,
+      close: 63
+    },
+    {
+      time: 1651510800000,
+      open: 65,
+      high: 65,
+      low: 65,
+      close: 65
+    },
+    {
+      time: 1651514400000,
+      open: 65,
+      high: 65,
+      low: 65,
+      close: 65
+    },
+    {
+      time: 1651518000000,
+      open: 65,
+      high: 65,
+      low: 65,
+      close: 65
+    },
+    {
+      time: 1651521600000,
+      open: 65,
+      high: 66.31,
+      low: 64.69,
+      close: 66.31
+    },
+    {
+      time: 1651525200000,
+      open: 66.31,
+      high: 67,
+      low: 66,
+      close: 66.31
+    },
+    {
+      time: 1651528800000,
+      open: 63.5,
+      high: 66,
+      low: 61,
+      close: 65
+    },
+    {
+      time: 1651532400000,
+      open: 65,
+      high: 65,
+      low: 65,
+      close: 65
+    },
+    {
+      time: 1651536000000,
+      open: 64.9,
+      high: 64.9,
+      low: 64.9,
+      close: 64.9
+    },
+    {
+      time: 1651543200000,
+      open: 66,
+      high: 66,
+      low: 65.8,
+      close: 65.8
+    },
+    {
+      time: 1651546800000,
+      open: 65.6,
+      high: 65.6,
+      low: 64.64,
+      close: 64.64
+    },
+    {
+      time: 1651550400000,
+      open: 64.1,
+      high: 64.1,
+      low: 62.6,
+      close: 62.6
+    },
+    {
+      time: 1651554000000,
+      open: 62.6,
+      high: 62.6,
+      low: 62,
+      close: 62
+    },
+    {
+      time: 1651557600000,
+      open: 61,
+      high: 61,
+      low: 60,
+      close: 60
+    },
+    {
+      time: 1651561200000,
+      open: 60,
+      high: 60,
+      low: 60,
+      close: 60
+    },
+    {
+      time: 1651564800000,
+      open: 60,
+      high: 60,
+      low: 59,
+      close: 59
+    },
+    {
+      time: 1651568400000,
+      open: 58.99,
+      high: 58.99,
+      low: 57,
+      close: 57
+    },
+    {
+      time: 1651572000000,
+      open: 57,
+      high: 60,
+      low: 57,
+      close: 60
+    },
+    {
+      time: 1651575600000,
+      open: 60,
+      high: 63.69,
+      low: 60,
+      close: 63.69
+    },
+    {
+      time: 1651579200000,
+      open: 63.69,
+      high: 63.69,
+      low: 62,
+      close: 62
+    },
+    {
+      time: 1651582800000,
+      open: 62,
+      high: 62,
+      low: 60,
+      close: 60
+    },
+    {
+      time: 1651586400000,
+      open: 60,
+      high: 60,
+      low: 58,
+      close: 58
+    },
+    {
+      time: 1651590000000,
+      open: 58,
+      high: 58,
+      low: 57.57,
+      close: 58
+    },
+    {
+      time: 1651593600000,
+      open: 58,
+      high: 58,
+      low: 58,
+      close: 58
+    },
+    {
+      time: 1651597200000,
+      open: 58,
+      high: 58,
+      low: 58,
+      close: 58
+    },
+    {
+      time: 1651600800000,
+      open: 58,
+      high: 58,
+      low: 58,
+      close: 58
+    },
+    {
+      time: 1651604400000,
+      open: 57,
+      high: 57,
+      low: 54.99,
+      close: 54.99
+    },
+    {
+      time: 1651608000000,
+      open: 55,
+      high: 57,
+      low: 55,
+      close: 57
+    },
+    {
+      time: 1651611600000,
+      open: 56.5,
+      high: 56.69,
+      low: 56.5,
+      close: 56.69
+    },
+    {
+      time: 1651615200000,
+      open: 56.69,
+      high: 56.69,
+      low: 56.69,
+      close: 56.69
+    },
+    {
+      time: 1651618800000,
+      open: 56.5,
+      high: 56.5,
+      low: 56.5,
+      close: 56.5
+    },
+    {
+      time: 1651622400000,
+      open: 56.5,
+      high: 56.5,
+      low: 56.5,
+      close: 56.5
+    },
+    {
+      time: 1651626000000,
+      open: 56.5,
+      high: 56.5,
+      low: 56.5,
+      close: 56.5
+    },
+    {
+      time: 1651629600000,
+      open: 56.5,
+      high: 56.5,
+      low: 54.69,
+      close: 56.4
+    },
+    {
+      time: 1651633200000,
+      open: 55,
+      high: 55,
+      low: 54.5,
+      close: 54.5
+    },
+    {
+      time: 1651636800000,
+      open: 55,
+      high: 55,
+      low: 55,
+      close: 55
+    },
+    {
+      time: 1651640400000,
+      open: 55,
+      high: 55,
+      low: 53,
+      close: 54
+    },
+    {
+      time: 1651644000000,
+      open: 54,
+      high: 55,
+      low: 54,
+      close: 54.9
+    },
+    {
+      time: 1651647600000,
+      open: 54.9,
+      high: 54.9,
+      low: 54.49,
+      close: 54.49
+    },
+    {
+      time: 1651651200000,
+      open: 54,
+      high: 54,
+      low: 53.9,
+      close: 53.9
+    },
+    {
+      time: 1651654800000,
+      open: 53.9,
+      high: 53.9,
+      low: 53.8,
+      close: 53.8
+    },
+    {
+      time: 1651658400000,
+      open: 53.5,
+      high: 53.5,
+      low: 52,
+      close: 52
+    },
+    {
+      time: 1651662000000,
+      open: 52,
+      high: 52,
+      low: 52,
+      close: 52
+    },
+    {
+      time: 1651665600000,
+      open: 51.25,
+      high: 51.25,
+      low: 48,
+      close: 48.5
+    },
+    {
+      time: 1651669200000,
+      open: 49.5,
+      high: 53,
+      low: 49.5,
+      close: 53
+    },
+    {
+      time: 1651672800000,
+      open: 52.99,
+      high: 63.9,
+      low: 52.99,
+      close: 63.9
+    },
+    {
+      time: 1651676400000,
+      open: 63.9,
+      high: 63.9,
+      low: 61,
+      close: 61
+    },
+    {
+      time: 1651680000000,
+      open: 63.9,
+      high: 63.9,
+      low: 63,
+      close: 63
+    },
+    {
+      time: 1651683600000,
+      open: 62,
+      high: 62,
+      low: 60.8,
+      close: 61
+    },
+    {
+      time: 1651687200000,
+      open: 61,
+      high: 62,
+      low: 58.5,
+      close: 58.5
+    },
+    {
+      time: 1651690800000,
+      open: 60,
+      high: 63,
+      low: 60,
+      close: 63
+    },
+    {
+      time: 1651694400000,
+      open: 63,
+      high: 63,
+      low: 63,
+      close: 63
+    },
+    {
+      time: 1651698000000,
+      open: 63,
+      high: 63,
+      low: 63,
+      close: 63
+    },
+    {
+      time: 1651701600000,
+      open: 63,
+      high: 63,
+      low: 62.9,
+      close: 62.9
+    },
+    {
+      time: 1651705200000,
+      open: 62.9,
+      high: 62.9,
+      low: 62,
+      close: 62
+    },
+    {
+      time: 1651708800000,
+      open: 62,
+      high: 62.9,
+      low: 62,
+      close: 62.9
+    },
+    {
+      time: 1651712400000,
+      open: 62,
+      high: 62,
+      low: 61,
+      close: 61.99
+    },
+    {
+      time: 1651716000000,
+      open: 61.99,
+      high: 61.99,
+      low: 60,
+      close: 61.99
+    },
+    {
+      time: 1651719600000,
+      open: 60,
+      high: 60,
+      low: 59.97,
+      close: 59.97
+    },
+    {
+      time: 1651723200000,
+      open: 59.97,
+      high: 59.97,
+      low: 59.97,
+      close: 59.97
+    },
+    {
+      time: 1651726800000,
+      open: 59.97,
+      high: 59.97,
+      low: 59.97,
+      close: 59.97
+    },
+    {
+      time: 1651730400000,
+      open: 59.97,
+      high: 59.97,
+      low: 59.97,
+      close: 59.97
+    },
+    {
+      time: 1651734000000,
+      open: 59.97,
+      high: 59.97,
+      low: 59.97,
+      close: 59.97
+    },
+    {
+      time: 1651737600000,
+      open: 59.97,
+      high: 59.97,
+      low: 59.97,
+      close: 59.97
+    },
+    {
+      time: 1651741200000,
+      open: 59.97,
+      high: 59.97,
+      low: 59.8,
+      close: 59.8
+    },
+    {
+      time: 1651744800000,
+      open: 59.8,
+      high: 59.8,
+      low: 59.5,
+      close: 59.5
+    },
+    {
+      time: 1651748400000,
+      open: 59.5,
+      high: 59.5,
+      low: 59.5,
+      close: 59.5
+    },
+    {
+      time: 1651752000000,
+      open: 59.5,
+      high: 59.5,
+      low: 59,
+      close: 59
+    },
+    {
+      time: 1651755600000,
+      open: 59,
+      high: 59.5,
+      low: 59,
+      close: 59
+    },
+    {
+      time: 1651759200000,
+      open: 58,
+      high: 58.5,
+      low: 58,
+      close: 58.5
+    },
+    {
+      time: 1651762800000,
+      open: 58.5,
+      high: 58.5,
+      low: 58.5,
+      close: 58.5
+    },
+    {
+      time: 1651766400000,
+      open: 58.5,
+      high: 58.5,
+      low: 57,
+      close: 57
+    },
+    {
+      time: 1651770000000,
+      open: 56,
+      high: 56,
+      low: 54.5,
+      close: 55
+    },
+    {
+      time: 1651773600000,
+      open: 55.4,
+      high: 55.4,
+      low: 52,
+      close: 52
+    },
+    {
+      time: 1651777200000,
+      open: 52,
+      high: 56.9,
+      low: 52,
+      close: 54.9
+    },
+    {
+      time: 1651780800000,
+      open: 56,
+      high: 56,
+      low: 56,
+      close: 56
+    },
+    {
+      time: 1651784400000,
+      open: 56,
+      high: 56,
+      low: 56,
+      close: 56
+    },
+    {
+      time: 1651788000000,
+      open: 56,
+      high: 56,
+      low: 56,
+      close: 56
+    },
+    {
+      time: 1651791600000,
+      open: 56,
+      high: 56,
+      low: 56,
+      close: 56
+    },
+    {
+      time: 1651795200000,
+      open: 56,
+      high: 58,
+      low: 56,
+      close: 58
+    },
+    {
+      time: 1651798800000,
+      open: 58,
+      high: 58,
+      low: 57.75,
+      close: 57.75
+    },
+    {
+      time: 1651802400000,
+      open: 57.75,
+      high: 57.75,
+      low: 56,
+      close: 56
+    },
+    {
+      time: 1651806000000,
+      open: 55,
+      high: 55,
+      low: 55,
+      close: 55
+    },
+    {
+      time: 1651809600000,
+      open: 55,
+      high: 55,
+      low: 55,
+      close: 55
+    },
+    {
+      time: 1651813200000,
+      open: 55,
+      high: 55,
+      low: 55,
+      close: 55
+    },
+    {
+      time: 1651816800000,
+      open: 55,
+      high: 55,
+      low: 55,
+      close: 55
+    },
+    {
+      time: 1651820400000,
+      open: 55,
+      high: 55,
+      low: 55,
+      close: 55
+    },
+    {
+      time: 1651824000000,
+      open: 55,
+      high: 55,
+      low: 55,
+      close: 55
+    },
+    {
+      time: 1651827600000,
+      open: 55,
+      high: 55,
+      low: 55,
+      close: 55
+    },
+    {
+      time: 1651831200000,
+      open: 55,
+      high: 55,
+      low: 55,
+      close: 55
+    },
+    {
+      time: 1651834800000,
+      open: 54.5,
+      high: 54.5,
+      low: 54.5,
+      close: 54.5
+    },
+    {
+      time: 1651838400000,
+      open: 56.5,
+      high: 56.5,
+      low: 56,
+      close: 56
+    },
+    {
+      time: 1651842000000,
+      open: 56,
+      high: 57,
+      low: 56,
+      close: 57
+    },
+  ]
